b2fc8dc0114c6c79516b26c1530ccfe79e97d855
gfshi <gfshi@bainainfo.com>
2017-02-13 10:28:43 +0800

    [Feature] add time format in timeutils.

--- utils/timeutils.py
+++ utils/timeutils.py
@@ -1,36 +1,38 @@
 #!/usr/bin/python
 import time
 from datetime import datetime, timedelta
+_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+_DATE_FORMAT = '%Y-%m-%d'
 
 
-def date2stamp(date_str):
-    timeArray = time.strptime(date_str, "%Y-%m-%d")
+def date2stamp(date_str, _format=_DATE_FORMAT):
+    timeArray = time.strptime(date_str, date_str)
     return time.mktime(timeArray)
 
 
-def stamp2date(stamp):
-    return time.strftime("%Y-%m-%d", time.localtime(stamp))
+def stamp2date(stamp, _format=_DATE_FORMAT):
+    return time.strftime(_format, time.localtime(stamp))
 
 
-def stamp2time(stamp):
-    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(stamp))
+def stamp2time(stamp, _format=_TIME_FORMAT):
+    return time.strftime(_format, time.localtime(stamp))
 
 
-def time2stamp(time_str):
-    timeArray = time.strptime(time_str, "%Y-%m-%d %H:%M:%S")
+def time2stamp(time_str, _format=_TIME_FORMAT):
+    timeArray = time.strptime(time_str, _format)
     return time.mktime(timeArray)
 
 
-def datetime2date(d):
+def datetime2date(d, _format=_DATE_FORMAT):
     if not isinstance(d, datetime):
         d = datetime.now()
-    return d.strftime("%Y-%m-%d")
+    return d.strftime(_format)
 
 
-def datetime2time(d):
+def datetime2time(d, _format=_TIME_FORMAT):
     if not isinstance(d, datetime):
         d = datetime.now()
-    return d.strftime("%Y-%m-%d %H:%M:%S")
+    return d.strftime(_format)
 
 
 def datetime2stamp(d):
@@ -39,10 +41,10 @@ def datetime2stamp(d):
     return time.mktime(d.timetuple())
 
 
-def time2datetime(time_str=None):
+def time2datetime(time_str=None, _format=_TIME_FORMAT):
     if not time_str:
         return datetime.now()
-    return datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S")
+    return datetime.strptime(time_str, _format)
 
 
 def stamp2datetime(stamp=None):
@@ -51,4 +53,4 @@ def stamp2datetime(stamp=None):
     return datetime.fromtimestamp(stamp)
 
 if __name__ == "__main__":
-    print time2stamp("2016-6-24 15:23:00")
+    print time2stamp("2016-6-24", '%Y-%m-%d')
